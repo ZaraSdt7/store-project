57190f6d85f6c010aa863f66cecf368cf53c1d04
zahra_sdt <zahra.st7373@gmail.com>
2023-03-29 12:07:29 +0430

    update product

--- app/http/controller/Admin/product.controler.js
+++ app/http/controller/Admin/product.controler.js
@@ -50,7 +50,7 @@ let data=CopyObject(req.body);
 data.images=ListOfImageForRequest(req?.files||[],req.body.fileUploadPath)
 data.feture=SetFeture(req.body);
 let BlackListFeild=Object.values(ProductBlackList);
-DeleteInvitedPropertyObject=(data,BlackListFeild)
+DeleteInvitedPropertyObject(data,BlackListFeild)
 const EditProduct=await ProductModel.updateOne({_id:product._id},{$set:data});
 if(EditProduct.modifiedCount==0) throw {status:HttpStatus.INTERNAL_SERVER_ERROR, message:"خطای داخلی"}
 return res.status(HttpStatus.OK).json({ 
